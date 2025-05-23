72f1bc25800f6d08bd029020a87358ce4b83e4ad
plgatpsi <plg@psi.edu>
2025-05-22 19:27:28 -0500

    added annoucements and links to homepage

---
+++ src/components/annoucements.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="annoucements">
+      <div class="updated">Updated: 22 May 2025</div>
+      <h4>Announcements</h4>
+      <p>This site is in Beta! Currently no data can be expected to be saved. Play. Give feedback. Get a preview of what's coming.</p>
+
+      <h4>Known Bugs <span style="font-size: small">email <a href="mailto:plg@psi.edu">plg@psi.edu</a> or mention on Discord</span></h4>
+      <ul>
+        <li>Hangs if cookies go stall or session expires</li>
+      </ul>
+
+      <h4>Major Missing Features </h4>
+      <ul>
+        <li>Tutorials</li>
+        <li>Stats on Profile Page</li>
+        <li>Lunar Melt: Ability to move marks</li>
+      </ul>
+      <h4>Minor Missing Features</h4>
+      <ul>
+        <li>Add "BETA" graphic to hero graphic</li>
+        <li>Graphics/ Icons are needed on homepage blue boxes</li>
+      </ul>
+    </div>
+    <div id="socials">
+      <h4>Opensource, Open Science</h4>
+      <p>All are welcome. Help us map worlds and develop new ways of doing science.</p>
+      <h4>Join a PSI Community thru <a href="https://cosmoquest.org">CosmoQuest</a></h4>
+      <p>Get space news, educational content, podcasts, and more through our donation-funded sibling program.</p>
+
+
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -5,6 +5,7 @@
     <HeroBanner></HeroBanner>
     <div class="page-layout__content">
       <Blurbs/>
+      <Announcements/>
     </div>
     <PageFooter />
   </div>
@@ -13,6 +14,7 @@
 <script setup>
 import HeroBanner from "@/components/hero-banner.vue";
 import Blurbs from "@/components/blurbs.vue";
+import Announcements from "@/components/annoucements.vue";
 import MobileNavBar from "@/components/navigation/mobile/mobile-nav-bar.vue";
 import NavBar from "@/components/navigation/desktop/nav-bar.vue";
 import PageFooter from "@/components/page-footer.vue";
