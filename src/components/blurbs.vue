44bda50687c33c29f6d4e4ea8a51894b406f2549
plgatpsi <plg@psi.edu>
2026-07-04 16:54:57 -0500

    Updating blurbs

--- src/components/blurbs.vue
+++ src/components/blurbs.vue
@@ -1,23 +1,25 @@
 <template>
 <div class="main-content" id="blurbs">
   <Blurb
-      title="Lunar Melt"
-      description="Help us map out ancient geologies on the Moon as we research what happens when asteroid impacts melt rock."
+      title="Map Lunar Features"
+      description="Help us map out ancient lunar geologies as we research what happens when asteroid impacts and melt
+                    flows move boulders, scatter rocks, and get cratered over time."
       image="https://moon-mappers.s3.us-east-2.amazonaws.com/Moon.png"
       link="do_science/lunar-melt"
   />
   <Blurb
-      title="Mars Mosaics"
-      description="Let's verify new algorithms make accurate Mars mosaics that provide clear views and pixel perfect coordinates."
+      title="Trace Lunar Flows"
+      description="Help us map out features form from or in impact melts on the Moon, including melt flows and channels, surface cracks,
+                    as well as pressure ridges."
+      image="https://moon-mappers.s3.us-east-2.amazonaws.com/Moon.png"
+      link="do_science/lunar-melt-flows"
+  />
+  <Blurb
+      title="Study Mars Mosaics"
+      description="Let's verify new algorithms make accurate Mars mosaics that provide clear views and pixel perfect coordinates. NOTE:
+                  we are currently awaiting new data. Stay tuned!"
       image="https://cosmoquest.s3.us-east-1.amazonaws.com/data/mosaics/Mars.png"
       link="do_science/mars-mosaic"
-  />
-
-  <Blurb
-      title="Looking Ahead"
-      description="New projects are under development. Stay tuned."
-      image="https://wm-web-assets.s3.us-east-2.amazonaws.com/Rocket.png"
-      link=""
   />
 </div>
 </template>
